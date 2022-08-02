ead01f9255b6dc64761b520191f8a547006419c2
Fer0rozco <Fernando.md@pm.me>
2022-08-02 11:17:43 -0500

    Ejercicios Js

---
+++ Ejercicio1.js
@@ -0,0 +1,35 @@
+//Ejercicio 1
+//10 boolean, 10 string, 10 number
+
+var percentageLeft = 10;
+var primeMinisterNumber = 10;
+var atomicNumberNeon = 10;
+var romanX = 10;
+var koranVowels = 10;
+var knockoutBox = 10;
+var greekDeca = 10;
+var metricSystem = 10;
+var fingersTotal = 10;
+var daysWeek = 7;
+
+var luzDeDia = true;
+var sueño = true;
+var noche = false;
+var ciego = true;
+var dolorEspalda = true;
+var aguaMty = false;
+var hambre = true;
+var lentes = true;
+var cafesito = true;
+var galletas = true;
+
+var name = "Juan";
+var lastName = "Perez";
+var municipio = "Guadalajara";
+var country = "Mexico";
+var planet = "Earth";
+var trabajo = "Programador";
+var salut = "hola"; 
+var vert = "verde";
+var bonjour = "buenos dias";
+var auRevoir = "adios";
